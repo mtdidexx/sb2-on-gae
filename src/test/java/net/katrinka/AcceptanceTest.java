d88927e8ea4e61b2f2de316b80c3592b7fefbc61
Mark Donahue <mark-donahue@idexx.com>
2018-03-29 11:24:31 -0400

    Now with a cron job

---
+++ src/test/java/net/katrinka/AcceptanceTest.java
@@ -0,0 +1,4 @@
+package net.katrinka;
+
+public class AcceptanceTest {
+}
---
+++ src/test/java/net/katrinka/acceptance/SayHelloApiTest.java
@@ -0,0 +1,41 @@
+package net.katrinka.acceptance;
+
+import io.restassured.RestAssured;
+import net.katrinka.AcceptanceTest;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+@Category(AcceptanceTest.class)
+public class SayHelloApiTest {
+    private String path = "/sayhello";
+
+    @BeforeClass
+    public static void restAssuredConfig() {
+//        RestAssured.baseURI = "http://localhost:8080";
+        RestAssured.baseURI = "https://lims-mdonahue-sb.appspot.com";
+    }
+
+    @Test
+    public void sayHelloApi() {
+        given()
+                .log().uri()
+                .auth().preemptive().basic("admin", "ch0colateCak3")
+                .when()
+                .get(path)
+                .then()
+                .assertThat().statusCode(200);
+    }
+
+    @Test
+    public void requiresAuth() {
+        when()
+                .get(path)
+                .then()
+                .assertThat().statusCode(401);
+
+    }
+}
